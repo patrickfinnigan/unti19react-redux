242f95ff807b68319ed18a07fa194fc5e9d2b7ff
patrickfinnigan <patrickfinnigan94@gmail.com>
2019-05-23 16:48:15 -0700

    first lesson in react isnt much

---
+++ practice/src/components/HelloDiv.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HelloDiv = () => {
+  const name = "Patrick";
+  return (
+    <div>
+      <h1>Hello World! my name is {name}</h1>
+      <h2>Things</h2>
+      <ul>
+        <li>have you heard of darwin?</li>
+        <li>because youre about to experience</li>
+        <li>my natural selection</li>
+      </ul>
+    </div>
+  );
+};
+// <h1>Hello World!</h1>;
+
+export default HelloDiv;
